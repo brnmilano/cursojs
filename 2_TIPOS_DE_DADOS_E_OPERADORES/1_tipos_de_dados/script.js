aba8cbceffab7f774346ff6b332c7ab323e9254a
Bruno Milano <brn.milano@hotmail.com>
2022-10-24 21:32:01 -0300

    Sessão 2 - Tipos de dados e operadores lógicos (Aula 1 a aula 4

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/1_tipos_de_dados/script.js
@@ -0,0 +1,10 @@
+// A função TYPEOF irá verificar o tipo de dado impresso no console;
+
+console.log(typeof 13);
+// Nessa situação, irá imprimir o tipo NUMBER;
+
+console.log(typeof "Bom dia");
+// Nessa situação, irá imprimir o tipo STRING;
+
+console.log(typeof true);
+// Nessa situação, irá imprimir o tipo BOOLEAN;

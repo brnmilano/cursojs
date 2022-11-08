a493fd056691d8e8b37d218aa6a67a99e776f2e0
Bruno Milano <brn.milano@hotmail.com>
2022-11-07 23:06:02 -0300

    Sessão 2 - Tipos de dados e Operadores (Completo)

--- 2_TIPOS_DE_DADOS_E_OPERADORES/6_blooleans_exemplos/script.js
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/6_blooleans_exemplos/script.js
@@ -3,7 +3,13 @@ console.log(5 > 3 && 3 === 2);
 console.log(3 == 3 && "Bruno" == "Bruno");
 console.log("Felipe" == "João" || false);
 console.log(!(true && true));
-console.log(!(!(true && true)));
+console.log(!!(true && true));
 
 console.log(true && true); //só retorna true se os dois lados forem true
 console.log(false || false); //o OU só retorna false se os dois lados forem false
+
+// operador ternário
+
+console.log(5 > 2 ? "É maior" : "Não é maior"); // Retorna um boolean
+console.log(false ? 5 : 4);
+console.log("Bruno" == "Bruno" ? "Oi Bruno" : "Não é o Bruno"); // Aqui é uma condicional

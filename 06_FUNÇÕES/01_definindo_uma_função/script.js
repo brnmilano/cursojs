755699580405b3535a4ddac4c8f112a7b0d62977
Bruno Milano <brnmilano.dev@gmail.com>
2022-11-22 22:25:48 -0300

    Funções

---
+++ 06_FUNÇÕES/01_definindo_uma_função/script.js
@@ -0,0 +1,20 @@
+function imprimirConsole() {
+  console.log("Olá mundo");
+}
+
+imprimirConsole();
+
+function imprimirNumero(num) {
+  console.log("O número é: " + num);
+}
+
+imprimirNumero(2);
+imprimirNumero(20);
+imprimirNumero(40);
+imprimirNumero(50);
+
+const numeroAleatorio = function () {
+    console.log(Math.random());
+}
+
+numeroAleatorio()
